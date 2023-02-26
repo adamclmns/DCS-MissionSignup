fc116ea973a875537bb51b47fb96b43747cdc85d
AdamClmns <adam@adamclmns.com>
2023-02-25 21:11:57 -0600

    Renamed ato.html to index.html so the view shows up at the root.

--- src/test/java/miz/signup/ATOTableMappingTest.java
+++ src/test/java/miz/signup/ATOTableMappingTest.java
@@ -1,7 +1,6 @@
 package miz.signup;
 
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import miz.signup.dto.ATO;
 import miz.signup.entities.AtoTable;
